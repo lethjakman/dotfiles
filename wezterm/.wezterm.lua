f355e561ca9192ee565891d9bd7f712676e03f73
Alex Kardos <alexander.kardos@gmail.com>
2024-04-15 09:48:26 -0600

    Add wezterm to bootstrapping

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,14 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.font = wezterm.font 'MesloLGM Nerd Font Mono'
+config.color_scheme = 'Night Owl (Gogh)'
+
+config.hide_tab_bar_if_only_one_tab = true
+
+-- and finally, return the configuration to wezterm
+return config
