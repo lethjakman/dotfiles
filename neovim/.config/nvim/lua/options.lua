9934104ed85cafc8ab9642117be35d90090de2e0
Alex Kardos <akardos@zscaler.com>
2024-06-05 07:57:22 -0600

    Enable relative line numbers

--- neovim/.config/nvim/lua/options.lua
+++ neovim/.config/nvim/lua/options.lua
@@ -4,3 +4,7 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
+vim.opt.relativenumber = true
+vim.o.statuscolumn = "%s %l %r"
