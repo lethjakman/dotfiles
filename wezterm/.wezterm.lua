98af04a611ddf31378c3b5f40985f28d81bd353a
Alex Kardos <akardos@zscaler.com>
2024-08-21 15:45:03 -0600

    Add some font choices

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,8 +5,10 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font 'MesloLGM Nerd Font Mono'
-config.color_scheme = 'Night Owl (Gogh)'
+config.font = wezterm.font "Terminess Nerd Font Mono"
+-- config.font = wezterm.font "FiraCode Nerd Font"
+-- config.font = wezterm.font "MesloLGM Nerd Font Mono"
+config.color_scheme = "Night Owl (Gogh)"
 
 config.hide_tab_bar_if_only_one_tab = true
 
