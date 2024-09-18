6aaa39e02ad8c5743a7f537c16075a98b4c39e83
Alex Kardos <akardos@zscaler.com>
2024-09-18 09:21:08 -0600

    use tsls instead of tsserver

--- neovim/.config/nvim/lua/configs/lspconfig.lua
+++ neovim/.config/nvim/lua/configs/lspconfig.lua
@@ -33,7 +33,7 @@ for _, lsp in ipairs(servers) do
 end
 
 -- typescript
-lspconfig.tsserver.setup {
+lspconfig.tsls.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
