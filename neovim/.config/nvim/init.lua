e27ef5635a25986af98101ac9371751458867caf
Alex Kardos <akardos@zscaler.com>
2025-06-04 08:13:01 -0600

    Add lua snippets

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,6 +1,9 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+-- vscode style snippets
+vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/lua/lua_snippets"
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
