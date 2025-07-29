f0fc7a67379759e70d33e6973cf493244a7b2705
Alex Kardos <akardos@zscaler.com>
2025-07-29 09:51:40 -0600

    Remove default mappings

--- neovim/.config/nvim/lua/mappings.lua
+++ neovim/.config/nvim/lua/mappings.lua
@@ -9,4 +9,7 @@ map("n", "<C-l>", ":TmuxNavigateRight<cr>", { desc = "switch window right" })
 map("n", "<C-j>", ":TmuxNavigateUp<cr>", { desc = "switch window down" })
 map("n", "<C-k>", ":TmuxNavigateDown<cr>", { desc = "switch window up" })
 
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("i", "jk", "<ESC>")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
