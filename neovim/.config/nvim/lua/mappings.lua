60f6ec3824f82fe160c6989aea2af37bcc4f6cfa
Alex Kardos <akardos@zscaler.com>
2025-07-29 09:51:22 -0600

    Add mappings for tmux navigator
    Overwrite the nvchad mappings with tmux navigator

--- neovim/.config/nvim/lua/mappings.lua
+++ neovim/.config/nvim/lua/mappings.lua
@@ -4,7 +4,9 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("n", "<C-h>", ":TmuxNavigateLeft<cr>", { desc = "switch window left" })
+map("n", "<C-l>", ":TmuxNavigateRight<cr>", { desc = "switch window right" })
+map("n", "<C-j>", ":TmuxNavigateUp<cr>", { desc = "switch window down" })
+map("n", "<C-k>", ":TmuxNavigateDown<cr>", { desc = "switch window up" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
