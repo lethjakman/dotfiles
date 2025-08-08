d7d01e893fed6861f88efd46c9b97dc01f544bd3
Alex Kardos <akardos@zscaler.com>
2025-08-08 08:18:48 -0600

    map Gitsigns blame

--- neovim/.config/nvim/lua/mappings.lua
+++ neovim/.config/nvim/lua/mappings.lua
@@ -9,6 +9,8 @@ map("n", "<C-l>", ":TmuxNavigateRight<cr>", { desc = "switch window right" })
 map("n", "<C-j>", ":TmuxNavigateDown<cr>", { desc = "switch window down" })
 map("n", "<C-k>", ":TmuxNavigateUp<cr>", { desc = "switch window up" })
 
+map("n", "<leader>gb", "<cmd>Gitsigns blame<cr>", { desc = "Git blame" })
+
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
 
