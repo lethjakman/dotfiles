c84c1dc2e4c80c11c6ae98a8603bda4d2f305b07
Alex Kardos <alexander.kardos@gmail.com>
2025-08-20 23:04:09 -0600

    Fix telescope depencies and remove snacks

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -15,6 +15,10 @@ return {
     end,
   },
   {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+  },
+  {
     "echasnovski/mini.nvim",
     version = "*",
     config = function()
@@ -637,29 +641,6 @@ return {
         },
       }
     end,
-  },
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    ---@type snacks.Config
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      -- bigfile = { enabled = true },
-      -- dashboard = { enabled = true },
-      -- explorer = { enabled = true },
-      -- indent = { enabled = true },
-      -- input = { enabled = true },
-      -- picker = { enabled = true },
-      -- notifier = { enabled = true },
-      -- quickfile = { enabled = true },
-      -- scope = { enabled = true },
-      -- scroll = { enabled = true },
-      -- statuscolumn = { enabled = true },
-      -- words = { enabled = true },
-    },
   },
   {
     "folke/todo-comments.nvim",
