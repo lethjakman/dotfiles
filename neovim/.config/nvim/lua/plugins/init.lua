3fca6d5d13a131e226d4e2d568003893c50f410f
Alex Kardos <alexander.kardos@gmail.com>
2025-08-20 22:46:02 -0600

    Update autosave

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -685,44 +685,28 @@ return {
     },
   },
   {
-    "pocco81/auto-save.nvim",
-    lazy = false,
-    config = function()
-      require("auto-save").setup {
-        enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-        execution_message = {
-          message = function() -- message to print on save
-            return ("AutoSave: saved at " .. vim.fn.strftime "%H:%M:%S")
-          end,
-          dim = 0.18, -- dim the color of `message`
-          cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-        },
-        trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
-        -- function that determines whether to save the current buffer or not
-        -- return true: if buffer is ok to be saved
-        -- return false: if it's not ok to be saved
-        condition = function(buf)
-          local fn = vim.fn
-          local utils = require "auto-save.utils.data"
-
-          if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-            return true -- met condition(s), can save
-          end
-          return false -- can't save
-        end,
-        write_all_buffers = false, -- write all buffers when the current one meets `condition`
-        debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
-        callbacks = { -- functions to be executed at different intervals
-          enabling = nil, -- ran when enabling auto-save
-          disabling = nil, -- ran when disabling auto-save
-          before_asserting_save = nil, -- ran before checking `condition`
-          before_saving = nil, -- ran before doing the actual save
-          after_saving = nil, -- ran after doing the actual save
-        },
-      }
-    end,
-    keys = {
-      { "<leader>as", "<cmd>ASToggle<cr>", desc = "Toggle autosave" },
+    "okuuva/auto-save.nvim",
+    version = "^1.0.0", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+      trigger_events = { -- See :h events
+        immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" }, -- vim events that trigger an immediate save
+        defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+        cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+      },
+      -- function that takes the buffer handle and determines whether to save the current buffer or not
+      -- return true: if buffer is ok to be saved
+      -- return false: if it's not ok to be saved
+      -- if set to `nil` then no specific condition is applied
+      condition = nil,
+      write_all_buffers = false, -- write all buffers when the current one meets `condition`
+      noautocmd = true, -- do not execute autocmds when saving
+      lockmarks = false, -- lock marks when saving, see `:h lockmarks` for more details
+      debounce_delay = 1000, -- delay after which a pending save is executed
+      -- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
+      debug = false,
     },
   },
 }
